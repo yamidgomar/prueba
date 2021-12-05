9ce9352e64ca321989420fffe45f0524cf85d755
yamid gonzalez <yamidgm@gmail.com>
2021-12-05 18:20:07 -0500

    actualizado desde android

--- express_dos/prueba_dos/network/routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const message = require('../component/messages/networt')
-
-const routes = (server) => {
-    server.use('/message', message)
-} 
-
-module.exports = routes
-
